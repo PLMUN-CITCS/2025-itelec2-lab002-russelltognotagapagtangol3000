dc7cd7297b6a2163e11622492cc7589cca9732c9
zach <tognorussell@gmail.com>
2025-02-27 19:20:40 +0800

    Submitting Python Session 03 - Exercise 01

--- problem_01.py
+++ problem_01.py
@@ -1,10 +1,21 @@
-# YOUR NAME
+# Russell Togno
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
 
 def main():
-    pass # replace this line with all of your code
+    # Introduce the program
+    print("Simple Calculator Program")
+    
+    # Obtain user input
+    num1 = int(input("Enter 1st number: "))
+    num2 = int(input("Enter 2nd number: "))
+
+    # Perform arithmetic operations and display results
+    print(f"The sum is {num1 + num2}")
+    print(f"The difference is {num1 - num2}")
+    print(f"The product is {num1 * num2}")
+    print(f"The quotient is {num1 / num2:.2f}")
 
 if __name__ == "__main__":
     main()
--- problem_02.py
+++ problem_02.py
@@ -1,10 +1,17 @@
-# YOUR NAME
+# Russell Togno
 # ITELEC2
 # Problem Set 01 - Problem 02
 # Square the Number Program
 
 def main():
-    pass # replace this line with all of your code
+    # Introduce the program
+    print("Square the Number Program")
+    
+    # Obtain user input
+    number = int(input("Enter a number: "))
+
+    # Compute and display the square of the number
+    print(f"The square of {number} is {number * number:.2f}")
 
 if __name__ == "__main__":
     main()
